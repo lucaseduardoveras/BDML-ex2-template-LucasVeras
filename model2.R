5e6677f9ab5eedc05b1ddcd9d50743c02b5a7e26
lucaseduardoveras <lucas_eduardo93@hotmail.com>
2025-08-16 11:40:41 -0500

    Update model2.R

--- model2.R
+++ model2.R
@@ -1,2 +1,43 @@
+## leverage 
+db_int <- db_int %>% mutate(leverage = hatvalues(linear_model))
+
+## residuals
+db_int <- db_int %>% mutate(residuals = linear_model$residuals)
+
+N <- nrow(db_int)
+db_int$id <- seq(1, N)
+
+a <- ggplot(db_int, aes(y = leverage, x = id, color = ofic_ingLab, shape = as.factor(gender))) +
+  geom_point() + # add points
+  theme_bw() + # black and white theme
+  labs(x = "Observations",  
+       y = "Leverage",
+       title = "") # labels
+
+b <- ggplot(db_int, aes(y = leverage, x = residuals)) +
+  geom_point() + # add points
+  theme_bw() + # black and white theme
+  labs(x = "Residuals",  
+       y = "Leverage",
+       title = "") # labels
+
+# Arrange the ggplot2 plots side by side using grid.arrange()
+grid.arrange(a, b, ncol = 2)
+
+p <- mean(db_int$leverage)
+p
+
+cutt <- 3*p
+cutt
+
+db_int2 <- db_int %>% 
+  dplyr::filter(leverage <= cutt)
+
+linear_model2 <- lm(totalHoursWorked ~ ofic_ingLab + nmenores + 
+                      nmenores*gender + H_Head + age + gender, data=db_int2)
+
+stargazer(linear_model, linear_model2, type="text",
+          covariate.labels=c("Mean Ocu Income","N under 18","Male",
+                             "Household Head","Age", "N under 18 x Male"))
 
 # Fill here the code to estimate the linear_model2 from the notebook
